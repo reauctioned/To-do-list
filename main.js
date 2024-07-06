2d4d8efec13b773c8aa1c09c83b57730f36e4f18
Saurav <neechanzy@gmail.com>
2024-07-06 11:34:31 +0545

    added list renders

---
+++ main.js
@@ -0,0 +1,23 @@
+let todoList = ['here','there']
+
+renderList()
+
+function renderList(){
+
+        let todoHTML = ''
+        
+        for (let i = 0; i < todoList.length; i++) {
+            let list = todoList[i]
+            let html = `<p>${list}</p>`
+        todoHTML += html
+    }
+
+    document.querySelector('.allList').innerHTML = todoHTML
+}
+
+function addTodo() {
+    let inpElement = document.querySelector('.todoName').value
+    todoList.push(inpElement)
+    document.querySelector('.todoName').value = ''
+  renderList()
+}
